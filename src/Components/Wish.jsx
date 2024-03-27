05bfb7ca6af1f69a922acd3589d69e223abb6005
Abhishek <116707298+The-Abhishek1@users.noreply.github.com>
2024-03-27 08:26:13 +0530

    Add files via upload

---
+++ src/Components/Wish.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Wish() {
+  return (
+    <div className="m-3 mt-0">
+      <h3 className=" p-2 text-center text-[14px] text-white -tracking-tighter italic">
+        "Happy Birthday to someone who fills my days with laughter and my heart
+        with warmth! 🎉 Your presence in my life is a gift I cherish every day.
+        May your birthday be as beautiful and bright as you are, surrounded by
+        love and happiness. Here's to celebrating you today and always! 🎂✨"
+      </h3>
+    </div>
+  );
+}
+
+export default Wish;
